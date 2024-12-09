8217f5a89a27a5e00a9c7ae6ff5403dea91362d7
Isbat Uddin <167951977+isbatuddin@users.noreply.github.com>
2024-12-09 20:53:37 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './index.html',
+    './css/tailwind.css',
+    './js/form.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
